f0f889515c23f7543efcf4976d0ba3a7eb20f133
Dip Kundu <102429238+dip-ayon@users.noreply.github.com>
2025-07-22 14:13:31 +0600

    Update landing-page.tsx

--- src/components/landing-page.tsx
+++ src/components/landing-page.tsx
@@ -79,7 +79,7 @@ export default function LandingPage() {
                 </div>
             </div>
             <div className="relative w-full h-64 md:h-96 rounded-lg overflow-hidden shadow-2xl">
-                <Image src="https://placehold.co/600x400.png" alt="Happy blood donors and recipients" layout="fill" objectFit="cover" data-ai-hint="blood donation community" />
+                <Image src="happydonor.jpeg" alt="Happy blood donors and recipients" layout="fill" objectFit="cover" data-ai-hint="blood donation community" />
             </div>
         </div>
       </section>
